c38d3f9e6de6353d823e9bf1bc712018b8b2ab86
dubhunter <the@dubhunter.com>
2015-07-20 02:11:12 -0700

    add volt filter and function helpers

---
+++ src/Talon/VoltFilterCollection.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Talon;
+
+use Phalcon\Text;
+use Phalcon\Mvc\View\Engine\Volt\Compiler as VoltCompiler;
+
+abstract class VoltFilterCollection {
+
+	/**
+	 * @param VoltCompiler $compiler
+	 */
+	public static function install($compiler) {
+		foreach (get_class_methods(get_called_class()) as $method) {
+			if ($method != __METHOD__) {
+				$compiler->addFilter(Text::uncamelize($method), function ($resolvedArgs, $exprArgs) use ($method) {
+					return get_called_class() . '::' . $method . '(' . $resolvedArgs . ')';
+				});
+			}
+		}
+	}
+}
