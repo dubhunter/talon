151db409e647fe393d7d50ca3f0e88ba2de6c3a8
dubhunter <the@dubhunter.com>
2015-08-06 00:30:29 -0700

    use a callback function instead

--- src/Talon/FilterCollection.php
+++ src/Talon/FilterCollection.php
@@ -13,7 +13,9 @@ abstract class FilterCollection {
 	public static function install($filter) {
 		foreach (get_class_methods(get_called_class()) as $method) {
 			if ($method != __METHOD__) {
-				$filter->add(Text::uncamelize($method), array(get_called_class(), $method));
+				$filter->add(Text::uncamelize($method), function ($value) use ($method) {
+					call_user_func(array(get_called_class(), $method), $value);
+				});
 			}
 		}
 	}
