c53c1c2e38cd014fee0d7d320cb873a010d78680
dubhunter <the@dubhunter.com>
2015-08-06 00:34:08 -0700

    make sure we return

--- src/Talon/FilterCollection.php
+++ src/Talon/FilterCollection.php
@@ -14,7 +14,7 @@ abstract class FilterCollection {
 		foreach (get_class_methods(get_called_class()) as $method) {
 			if ($method != __METHOD__) {
 				$filter->add(Text::uncamelize($method), function ($value) use ($method) {
-					call_user_func(array(get_called_class(), $method), $value);
+					return call_user_func(array(get_called_class(), $method), $value);
 				});
 			}
 		}
